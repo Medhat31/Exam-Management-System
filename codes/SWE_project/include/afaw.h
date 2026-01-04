f12730b1bda98ef645eb66fed0269c7b24e45984
Medhat31 <medhatkhalil340@gmail.com>
2026-01-04 22:22:06 +0200

    Organized the files

---
+++ codes/SWE_project/include/afaw.h
@@ -0,0 +1,16 @@
+#ifndef AFAW_H
+#define AFAW_H
+
+
+class afaw
+{
+    public:
+        afaw();
+        virtual ~afaw();
+
+    protected:
+
+    private:
+};
+
+#endif // AFAW_H
---
+++ codes/SWE_project/src/DB_Manipulation.cpp
@@ -0,0 +1,35 @@
+#include "DB_Manipulation.h"
+
+
+void executeQuery(sqlite3* db, string sql) {
+    char* messaggeError;
+    int exit = sqlite3_exec(db, sql.c_str(), NULL, 0, &messaggeError);
+
+    if (exit != SQLITE_OK) {
+        cerr << "Error: " << messaggeError << endl;
+        sqlite3_free(messaggeError);
+    } else {
+        cout << "Query executed successfully." << endl;
+    }
+}
+
+// First, make sure you have the printer defined
+static int printData(void* NotUsed, int argc, char** argv, char** azColName) {
+    for (int i = 0; i < argc; i++) {
+        cout << azColName[i] << ": " << (argv[i] ? argv[i] : "NULL") << "  ";
+    }
+    cout << endl;
+    return 0;
+}
+
+// Second, the new function for selecting
+void executeSelect(sqlite3* db, string sql) {
+    char* error;
+    // NOTICE: We replaced NULL with 'printData'
+    int exit = sqlite3_exec(db, sql.c_str(), printData, 0, &error);
+
+    if (exit != SQLITE_OK) {
+        cerr << "Error: " << error << endl;
+        sqlite3_free(error);
+    }
+}
